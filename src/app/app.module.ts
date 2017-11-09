e4ba86840abafcc0ee91e3da3e416daa9b47be76
Pascal Andersson <anderssonpascal@gmail.com>
2017-11-09 21:22:09 +0100

    Added: db and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
+// External libs / components. -> @NgModule imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from './../environments/environment';
 
+// Generated components. -> @NgModule declarations
 import { AppComponent } from './app.component';
+import { BrowseComponent } from './components/browse/browse.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddComponent } from './components/add/add.component';
+import { DetailsComponent } from './components/details/details.component';
+
+
+// Services. -> @NgModule providers
+import { DbService } from './services/db.service';
+
+const appRouts = [
+  { path: '', component: HomeComponent },
+  { path: 'browse', component: BrowseComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'details', component: DetailsComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowseComponent,
+    HomeComponent,
+    AddComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouts),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [
+    DbService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDgWqVjpgN92u_4wuKZkOXtQ0V-0CmPo1M",
+    authDomain: "spothunt-e4fe3.firebaseapp.com",
+    databaseURL: "https://spothunt-e4fe3.firebaseio.com",
+    projectId: "spothunt-e4fe3",
+    storageBucket: "spothunt-e4fe3.appspot.com",
+    messagingSenderId: "133844939406"
+  }
 };
