a1151c85dc835a826b2bafbf636337ee1faeadd5
Unknown <robin.mik23@gmail.com>
2017-11-21 13:15:51 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/app/components/details/details.component.spec.ts
+++ src/app/components/details/details.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 import { DetailsComponent } from './details.component';
 
 describe('DetailsComponent', () => {
--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-details',
+  selector: 'ngbd-carousel-config',
   templateUrl: './details.component.html',
+  providers: [NgbCarouselConfig],
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = true;
+    config.keyboard = true;
+   }
 
   ngOnInit() {
   }
