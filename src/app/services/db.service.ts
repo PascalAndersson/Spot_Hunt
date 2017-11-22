1385c5bcd03d7aab875857743e71656e632e12e5
Pascal Andersson <anderssonpascal@gmail.com>
2017-11-22 12:17:21 +0100

    Added recently spots slice.

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -5,40 +5,25 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DbService {
-
   databaseRef: AngularFireList<any>;
   firebase: any;
-
   constructor(
     private afDb: AngularFireDatabase
   ) {
     this.databaseRef = afDb.list('spots');
     this.firebase = firebase;
-   }
-
-   public addSpot(spot: any) {
+  }
+  public addSpot(spot: any) {
     this.databaseRef.push(spot);
   }
-
   public getAllSpots() {
-    return this.afDb.list('spots').valueChanges();
+    return this.databaseRef.valueChanges();
   }
+  public getSpotsIncludingKey() {
+    return this.databaseRef.snapshotChanges();
 
-  public getSpotsIncludingKey(){
-    return this.databaseRef.snapshotChanges().map( changes => {
-      return changes.map( c => ({
-        key: c.payload.key, ...c.payload.val()
-      }));
-    });
   }
-
   public getSpotByKey(key: string) {
     return this.afDb.object('spots/' + key).valueChanges();
   }
 }
-
-
-
-
-
-
